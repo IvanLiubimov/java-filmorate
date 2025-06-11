f810110e14c3526813c12d28c0e53aedf2156470
Ivan Liubimov <lordolub@gmail.com>
2025-06-12 02:50:41 +0300

    First commit || Likes and Friends added, new logic added ||

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
