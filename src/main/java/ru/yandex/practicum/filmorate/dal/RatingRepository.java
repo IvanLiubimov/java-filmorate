b2b7d58b5b4d891a01377dbf68337942403bbd6c
Ivan Liubimov <lordolub@gmail.com>
2025-07-01 04:54:55 +0300

    First commit || Database added ||

--- src/main/java/ru/yandex/practicum/filmorate/dal/RatingRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/RatingRepository.java
@@ -5,25 +5,23 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Rating;
 import ru.yandex.practicum.filmorate.validator.FilmValidator;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Repository
 public class RatingRepository extends BaseRepository {
 
     FilmValidator filmValidator = new FilmValidator();
+
     public RatingRepository(JdbcTemplate jdbcTemplate,  @Qualifier("ratingMapper") RowMapper<Rating> mapper) {
         super(jdbcTemplate, mapper);
     }
 
-    private final String FIND_ALL_RATING = "SELECT r.* FROM rating r";
-    private final String FIND_RATING_BY_ID = "SELECT r.* FROM rating r WHERE id = ?";
+    private static final String FIND_ALL_RATING = "SELECT r.* FROM rating r";
+    private static final String FIND_RATING_BY_ID = "SELECT r.* FROM rating r WHERE id = ?";
 
     public Collection<Rating> findAllGenres() {
         return getAll(FIND_ALL_RATING);
