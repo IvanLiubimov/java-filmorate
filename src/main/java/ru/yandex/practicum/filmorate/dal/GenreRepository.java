42b3bae04a04c5118034c4ba91eea2618e8bc259
Ivan Liubimov <lordolub@gmail.com>
2025-07-01 04:52:09 +0300

    First commit || Database added ||

--- src/main/java/ru/yandex/practicum/filmorate/dal/GenreRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/GenreRepository.java
@@ -13,8 +13,8 @@ import java.util.Optional;
 @Repository
 public class GenreRepository extends BaseRepository {
 
-    private final String FIND_ALL_GENRES = "SELECT g.* FROM genres g";
-    private final String FIND_GENRE_BY_ID = "SELECT g.* FROM genres g WHERE genre_id = ?";
+    private static final String FIND_ALL_GENRES = "SELECT g.* FROM genres g";
+    private static final String FIND_GENRE_BY_ID = "SELECT g.* FROM genres g WHERE genre_id = ?";
 
     public GenreRepository(JdbcTemplate jdbcTemplate,  @Qualifier("genreMapper") RowMapper<Genre> mapper) {
         super(jdbcTemplate, mapper);
