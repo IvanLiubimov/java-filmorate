a6efa200d6686fd3369ba866609f09afd9db4155
Ivan Liubimov <lordolub@gmail.com>
2025-07-01 04:59:13 +0300

    First commit || Database added ||

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.validator.FilmValidator;
 
-import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
