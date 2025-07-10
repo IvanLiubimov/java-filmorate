4645a967d346aef4f80ae346a618ca521042bcc7
Ivan Liubimov <lordolub@gmail.com>
2025-07-10 17:55:22 +0300

    First commit || Directors added || first step NO integration in films

---
+++ src/main/java/ru/yandex/practicum/filmorate/dal/DirectorRepository.java
@@ -0,0 +1,53 @@
+package ru.yandex.practicum.filmorate.dal;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.Director;
+import java.util.Collection;
+import java.util.Optional;
+
+@Repository
+public class DirectorRepository extends BaseRepository<Director>{
+
+    public DirectorRepository(JdbcTemplate jdbcTemplate,  @Qualifier("directorMapper") RowMapper<Director> mapper) {
+        super(jdbcTemplate, mapper);
+    }
+
+    private static final String FIND_ALL_DIRECTORS = "SELECT dir.* FROM directors dir";
+    private static final String CREATE_DIRECTOR =
+            "INSERT INTO directors (name) VALUES (?)";
+    private static final String UPDATE_QUERY =
+            "UPDATE directors SET name = ? WHERE id = ?";
+    private static final String GET_DIRECTOR_BY_ID = "SELECT dir.* FROM directors dir WHERE id = ?";
+
+    public Collection<Director> getAllDirectors() {
+        return getAll(FIND_ALL_DIRECTORS);
+    }
+
+    public Optional<Director> getDirectorById(Long id) {
+        return findOne(GET_DIRECTOR_BY_ID, id);
+    }
+
+    public Director createDirector(Director director) {
+        long id = create(CREATE_DIRECTOR,
+                director.getName());
+        director.setId(id);
+        return director;
+    }
+
+    public Director updateDirector(Director director) {
+        update(UPDATE_QUERY,
+                director.getName(),
+                director.getId()
+        );
+        return director;
+    }
+
+    public void deleteDirector (Long id) {
+        String query = "DELETE FROM directors WHERE id = ?";
+        jdbcTemplate.update(query, id);
+    }
+
+}
