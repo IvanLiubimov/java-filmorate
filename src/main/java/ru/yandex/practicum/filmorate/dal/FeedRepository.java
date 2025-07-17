1272d28f54d28b699cf4447cf417f89efbb40274
Kirill Krasikov <c.krasikoff@yandex.ru>
2025-07-17 16:00:06 +0300

    Разрешение конфликИсправление чек-стайла

--- src/main/java/ru/yandex/practicum/filmorate/dal/FeedRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FeedRepository.java
@@ -1,20 +1,16 @@
 package ru.yandex.practicum.filmorate.dal;
 
 
-
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.FeedEvent;
-import ru.yandex.practicum.filmorate.model.enums.FeedEventType;
 import ru.yandex.practicum.filmorate.model.enums.FeedEventOperation;
+import ru.yandex.practicum.filmorate.model.enums.FeedEventType;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.Collection;
 
 @Repository
@@ -27,16 +23,6 @@ public class FeedRepository {
         return jdbcTemplate.query(sql, this::mapRowToFeedEvent, userId);
     }
 
-    /*public void save(FeedEvent event) {
-        String sql = "INSERT INTO user_feeds (user_id, event_type, operation, entity_id, timestamp) " +
-                "VALUES (?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sql,
-                event.getUserId(),
-                event.getEventType().name(),
-                event.getOperation().name(),
-                event.getEntityId(),
-                event.getTimestamp());
-    }*/
 
     public void save(FeedEvent event) {
         jdbcTemplate.update(
@@ -50,17 +36,7 @@ public class FeedRepository {
         );
     }
 
-    /*private FeedEvent mapRowToFeedEvent(ResultSet rs, int rowNum) throws SQLException {
-        return FeedEvent.builder()
-                .eventId(rs.getLong("event_id"))
-                .userId(rs.getLong("user_id"))
-                .eventType(FeedEventType.valueOf(rs.getString("event_type")))
-                .operation(FeedEventOperation.valueOf(rs.getString("operation")))
-                .entityId(rs.getLong("entity_id"))
-                .timestamp(rs.getLong("timestamp"))
-                .build();
-    }*/
-
+    
     private FeedEvent mapRowToFeedEvent(ResultSet rs, int rowNum) throws SQLException {
         return FeedEvent.builder()
                 .eventId(rs.getLong("event_id"))
